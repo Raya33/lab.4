8162e47a5cc1bb8dc5b7f12ece339e8cd3c56904
Raya33 <rayaabudalbouh@gmail.com>
2020-12-24 19:34:25 +0200

    roses

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-console.log('welcome to my page');
+alert('this store open at 10 ma');
 
- alert ('this store open at 10 ma');
-var roseName= prompt('enter the name');
+var answer = prompt('Do you love Roses ,enter yes/no');
+
+document.write('<p>' + answer + '</p>');
+
+while (answer !== 'yes' && answer !== 'no') {
+    answer = prompt('Do you love Roses ,enter yes/no');
+}
+
+
+
+if (answer === 'yes') {
+    alert('welcome to our store');
+
+}
+else if (answer === 'no') {
+    alert('this is not your place');
+}
+     var photo = "img src=https://s3.amazonaws.com/cdn.brecks.com/images/800/75696.jpg"
+     var photoloop = prompt('write down number from 1-5')
+for (photoloop < 0; photoloop>=5; ){photo}
+
+    document.write("<img src ='https://s3.amazonaws.com/cdn.brecks.com/images/800/75696.jpg'>");
